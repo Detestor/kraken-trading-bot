5dc27f29c2fc9851c2ea452a187fa46cdb4688f4
Detestor <markuswarin@gmail.com>
2026-03-10 20:09:33 +0100

    Delete core directory

--- core/datafeed.py
+++
@@ -1,7 +0,0 @@
-import ccxt, pandas as pd
-class DataFeed:
-    def __init__(self):
-        self.ex = ccxt.kraken({"enableRateLimit": True})
-    def fetch_ohlcv(self, symbol, timeframe="1m", limit=500):
-        ohlcv = self.ex.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
-        return pd.DataFrame(ohlcv, columns=["ts","open","high","low","close","volume"])
