e52260f8de1cfe479e5bd16d7005b7a6d8b3693f
Detestor <markuswarin@gmail.com>
2026-03-14 13:04:06 +0100

    Delete core directory

--- core/config.py
+++
@@ -1,7 +0,0 @@
-
-import os, yaml
-def load_config(path="config.yaml"):
-    if not os.path.exists(path):
-        return {}
-    with open(path, "r", encoding="utf-8") as f:
-        return yaml.safe_load(f) or {}
--- core/profit.py
+++
@@ -1,3 +0,0 @@
-
-def break_even_sell(entry_price, fee_rate, buffer_rate):
-    return entry_price * (1 + (2 * fee_rate) + buffer_rate)
